ec3f875a31f59744061940aa2bb065578aeb4f8d
Given <gkibanzait@gmail.com>
2024-01-02 15:09:04 +0200

    added confetti and reset counter on matching random number!

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,8 +1,34 @@
-import { useState } from 'react';
-import './counter.css'
+import { useEffect, useState } from 'react';
+import confetti from '../assets/scripts/confetti';
+import './counter.css';
+
+const getRandomNumber = () => {
+    return Math.floor(Math.random() * 15) + 1;
+};
+
 
 export default function Counter(){
     const [count, incrementCount] = useState(0);
+    const [randomNumber, setRandomNumber] = useState(getRandomNumber());
+
+    console.log("this is confetti:");
+    console.log(confetti);
+
+    useEffect(()=>{
+        // generate  a random number between 1 and 15
+
+        if (count === randomNumber) {
+            confetti.start();
+            setTimeout(() => {
+                confetti.stop();
+                setRandomNumber(getRandomNumber());
+                incrementCount(0);
+            }, 5000); // Stop the confetti effect after 5 seconds
+        }
+
+    }, [count]);
+
+    
 
     const btnClick = (event)=>{
         incrementCount(count+1);
